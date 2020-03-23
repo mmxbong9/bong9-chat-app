e41aeb405bd405e3ccca8c7794a4a12db47f22cb
mmxbong9 <mmxbong9@gmail.com>
2020-03-23 15:14:44 +0900

    Express Server Serve the static files from the React app

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
